33779c9a6caabe0f3fe2af5530ae8915a58cac96
davidblancolopez <daviddbl94@gmail.com>
2017-04-07 17:44:53 +0200

    Cambios

--- src/exercici/pkg1/m9/uf1/SeguretatAES.java
+++ src/exercici/pkg1/m9/uf1/SeguretatAES.java
@@ -1,24 +1,46 @@
-
 package exercici.pkg1.m9.uf1;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
-
+import javax.crypto.spec.SecretKeySpec;
+import static sun.security.krb5.internal.ktab.KeyTabConstants.keySize;
 
 public class SeguretatAES {
+    private final int keySize;
+    private final String ALGORITME = "AES";
+
     
     
-    public void xifrarFitxer(String fitxer, SecretKey clau){
-        
+    public SeguretatAES(int keySize) {
+        this.keySize = keySize;
     }
     
-    public void desxifrarFitxer(String fitxerEncriptat, SecretKey clave, String fitxerDesencriptat){
-        
-    }
     
-    public SecretKey generarClau(){
+    public SecretKey generarClau() {
         SecretKey clau = null;
-        
+
+        if ((keySize == 128) || (keySize == 192) || (keySize == 256)) {
+            try {
+                KeyGenerator kgen = KeyGenerator.getInstance(ALGORITME);
+                kgen.init(keySize);
+                clau = kgen.generateKey();
+            } catch (NoSuchAlgorithmException ex) {
+                System.err.println("Generador no disponible.");
+            }
+        }
         return clau;
     }
     
+    
+    public void xifrarFitxer(String fitxer, SecretKey clau) {
+
+    }
+
+    public void desxifrarFitxer(String fitxerEncriptat, SecretKey clave, String fitxerDesencriptat) {
+
+    }
+
 }
