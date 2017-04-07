e76c05098ffccdfdf7f8768fbd34ae4a827230f8
davidblancolopez <daviddbl94@gmail.com>
2017-04-07 17:37:58 +0200

    Cambios

--- src/exercici/pkg1/m9/uf1/SeguretatAES.java
+++ src/exercici/pkg1/m9/uf1/SeguretatAES.java
@@ -1,7 +1,24 @@
 
 package exercici.pkg1.m9.uf1;
 
+import javax.crypto.SecretKey;
+
 
 public class SeguretatAES {
+    
+    
+    public void xifrarFitxer(String fitxer, SecretKey clau){
+        
+    }
+    
+    public void desxifrarFitxer(String fitxerEncriptat, SecretKey clave, String fitxerDesencriptat){
+        
+    }
+    
+    public SecretKey generarClau(){
+        SecretKey clau = null;
+        
+        return clau;
+    }
     
 }
