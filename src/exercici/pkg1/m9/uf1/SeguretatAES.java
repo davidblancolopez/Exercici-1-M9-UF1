30bb983cde6479948fa09358194af2fb2f055db8
davidblancolopez <daviddbl94@gmail.com>
2017-04-07 17:53:51 +0200

    Cambios

--- src/exercici/pkg1/m9/uf1/SeguretatAES.java
+++ src/exercici/pkg1/m9/uf1/SeguretatAES.java
@@ -1,5 +1,6 @@
 package exercici.pkg1.m9.uf1;
 
+import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
@@ -9,36 +10,55 @@ import javax.crypto.spec.SecretKeySpec;
 import static sun.security.krb5.internal.ktab.KeyTabConstants.keySize;
 
 public class SeguretatAES {
+
     private final int keySize;
     private final String ALGORITME = "AES";
 
-    
-    
     public SeguretatAES(int keySize) {
         this.keySize = keySize;
     }
-    
-    
-    public SecretKey generarClau() {
+
+    /**
+     * Metodo para generar la clave.
+     *
+     * @param contrasenya
+     * @param tamany
+     * @return
+     */
+    public SecretKey generarClau(String contrasenya, int tamany) throws UnsupportedEncodingException {
         SecretKey clau = null;
 
-        if ((keySize == 128) || (keySize == 192) || (keySize == 256)) {
+        if ((tamany == 128) || (tamany == 192) || (tamany == 256)) {
             try {
-                KeyGenerator kgen = KeyGenerator.getInstance(ALGORITME);
-                kgen.init(keySize);
-                clau = kgen.generateKey();
+                byte[] data = contrasenya.getBytes("UTF-8");
+                MessageDigest md = MessageDigest.getInstance("SHA-256");
+                byte[] hash = md.digest(data);
+                byte[] key = Arrays.copyOf(hash, tamany / 8);
+                clau = new SecretKeySpec(key, "AES");
             } catch (NoSuchAlgorithmException ex) {
                 System.err.println("Generador no disponible.");
             }
         }
         return clau;
     }
-    
-    
+
+    /**
+     * Metodo para cifrar un fichero.
+     *
+     * @param fitxer
+     * @param clau
+     */
     public void xifrarFitxer(String fitxer, SecretKey clau) {
 
     }
 
+    /**
+     * Metodo para descifrar un fichero.
+     *
+     * @param fitxerEncriptat
+     * @param clave
+     * @param fitxerDesencriptat
+     */
     public void desxifrarFitxer(String fitxerEncriptat, SecretKey clave, String fitxerDesencriptat) {
 
     }
